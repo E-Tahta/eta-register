57c84975a57727acea39eb0e598cf51f73d6a762
Yunusemre Şentürk <yunusemre.senturk@pardus.org.tr>
2016-06-23 23:52:39 +0300

    Seperate directories

---
+++ server/etaRegister.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by yunusem.
+ * Developer: Yunusemre Şentürk 
+ * Date: 23/6/16
+ * Time: 12:06 AM
+ */
+header('Content-type: application/json');
+setlocale(LC_CTYPE, 'nl_BE.utf8');
+
+
+
+
+?>
